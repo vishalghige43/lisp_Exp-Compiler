ec80c75b4447e955826ef3aee64274d057308cfe
Vishal Ghige <vishalghige@Vishals-MacBook-Air.local>
2023-10-25 17:12:45 +0530

    Added tokenizer.print

--- compiler.h
+++ compiler.h
@@ -4,16 +4,19 @@
 // 4. Code Generation
 // return cCode;
 #include"tokenizer.h"
-void compile(string str){
-    /* 1. Lexical Analysis -
-    Breaks the input code (string) into the basic syntax 
-    of the language (array of objects)*/
-    vector<token>output;
-    output=tokenizer(str);
+void disply_tokens(const vector<token> &output){
     for(int i=0;i<output.size();i++){
         cout<<"  { "<<endl;
         cout<<"      type: \""<<output[i].type<<"\""<<endl;
         cout<<"      value: \""<<output[i].value<<"\""<<endl;
         cout<<"  }"<<endl;
     }
+}
+void compile(string str){
+    /* 1. Lexical Analysis -
+    Breaks the input code (string) into the basic syntax 
+    of the language (array of objects)*/
+    vector<token>output;
+    output=tokenizer(str);
+    disply_tokens(output); 
 }
